1170973456ca1fc6becd9a4774f47c6be520d113
Alok Kumar Verma <alok.mcaet@gmail.com>
2023-02-23 09:38:34 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import Login from './Components/Login/Login';
 import './App.css';
-
+import Faceprod from "../src/Pages/Faceprod"
 function App() {
   return (
     <div className="App">
+      <Faceprod />
     </div>
   );
 }
